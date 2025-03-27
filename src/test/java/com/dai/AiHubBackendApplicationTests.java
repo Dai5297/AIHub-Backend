2c45a9e7051a98b9570345f2903b72fefab8852f
Dai <15160255297@163.com>
2025-03-27 20:17:14 +0800

    feat: 初始化 AIHub-Backend项目
    - 创建项目结构和基本配置
    - 添加 Spring Boot 应用程序入口类和测试类- 配置 application.yml 文件- 创建 pom.xml 文件，定义项目依赖和构建配置
    - 添加 .gitignore 文件，忽略不必要的文件和目录

---
+++ src/test/java/com/dai/AiHubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiHubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
